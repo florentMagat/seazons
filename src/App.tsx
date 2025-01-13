ecfa28768803d48ba1d61aa34663a247253fcac5
florentMagat <adressemail22000@gmail.com>
2025-01-14 00:22:32 +0100

    install react + typescript + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-50">
+      <h1 className="text-4xl font-bold text-red-500">
+        Seazons
+      </h1>
+    </div>
+  );
+}
+
+export default App;
